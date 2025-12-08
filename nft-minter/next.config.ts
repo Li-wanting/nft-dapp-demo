429034ba0440a3ef3f5ea566a00231c3380c9e21
liwanting <liwanting0813@gmail.com>
2025-12-08 18:53:24 +0800

    fix: next config

--- nft-minter/next.config.ts
+++ nft-minter/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer }) {
+    // 忽略 thread-stream/test 文件
+    if (!isServer) {
+      config.module.rules.push({
+        test: /node_modules[\\/]thread-stream[\\/]test/,
+        loader: "ignore-loader",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
